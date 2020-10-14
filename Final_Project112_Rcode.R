a35835705e536c11575c525087f9200f4e57b2e1
EthanX-Deutsch <ethanxdeutsch@gmail.com>
2020-10-14 08:44:20 -0500

    New cleaned data set

--- Final_Project112_Rcode.R
+++ Final_Project112_Rcode.R
@@ -4,4 +4,7 @@ library(tidyverse)
 data <- read_csv("data.csv")
 
 cleaned_fifa_data <- data %>%
-  select(Name, Age, Nationality, Overall, Club, Value)
+  select(-X1, -ID, -Flag, -Potential, -`Club Logo`, -Wage, -Special, -`International Reputation`, -`Weak Foot`, -`Skill Moves`,
+         -`Work Rate`, -`Body Type`, -`Real Face`, -`Jersey Number`, -Joined, -`Loaned From`, -`Contract Valid Until`,
+         -LS, -ST, -RS, -LW, -LF, -CF, -RF, -RW, -LAM, -CAM, -RAM, -LCM, -CM, -RCM, -RM, -LB, -LM, -LCM, -CM, -RCM, 
+         -RM, -LWB, -LDM, -CDM, -RDM, -RWB, -LCB, -CB, -RCB, -RB, -`Release Clause`)
