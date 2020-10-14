c2f15f1e34bff538224a3c6eaf80462fea231469
EthanX-Deutsch <ethanxdeutsch@gmail.com>
2020-10-14 08:54:50 -0500

    Mission Statement

--- Final_Project112_Rcode.R
+++ Final_Project112_Rcode.R
@@ -8,3 +8,6 @@ cleaned_fifa_data <- data %>%
          -`Work Rate`, -`Body Type`, -`Real Face`, -`Jersey Number`, -Joined, -`Loaned From`, -`Contract Valid Until`,
          -LS, -ST, -RS, -LW, -LF, -CF, -RF, -RW, -LAM, -CAM, -RAM, -LCM, -CM, -RCM, -RM, -LB, -LM, -LCM, -CM, -RCM, 
          -RM, -LWB, -LDM, -CDM, -RDM, -RWB, -LCB, -CB, -RCB, -RB, -`Release Clause`)
+
+#Mission Statement: Finding Player Optimization for different players and teams to see which playing style fits to a
+# certain Fifa player, whether they likes Strong players, fast players, good passing, good dribbling, etc.
