90f7412a415b942e9642f6523b4f56d878f88af9
jcaskey1 <jcaskey1@macalester.edu>
2020-10-15 10:03:32 -0500

    Lisa's fix

--- Final_Project112_Rcode.R
+++ Final_Project112_Rcode.R
@@ -8,81 +8,97 @@ cleaned_fifa_data <- data %>%
   select(-X1, -ID, -Flag, -Potential, -`Club Logo`, -Wage, -Special, -`International Reputation`, -`Weak Foot`, -`Skill Moves`,
          -`Work Rate`, -`Body Type`, -`Real Face`, -`Jersey Number`, -Joined, -`Loaned From`, -`Contract Valid Until`,
          -LS, -ST, -RS, -LW, -LF, -CF, -RF, -RW, -LAM, -CAM, -RAM, -LCM, -CM, -RCM, -RM, -LB, -LM, -LCM, -CM, -RCM, 
-         -RM, -LWB, -LDM, -CDM, -RDM, -RWB, -LCB, -CB, -RCB, -RB, -`Release Clause`)
+         -RM, -LWB, -LDM, -CDM, -RDM, -RWB, -LCB, -CB, -RCB, -RB, -`Release Clause`) %>%
+  filter(Club %in% c("Arsenal", "Brighton & Hove Albion", 
+                     "Bournemouth", "Burnley", "Cardiff City", 
+                     "Chelsea", "Crystal Palace", "Everton",
+                     "Fulham", "Huddersfield Town", "Leicester City", 
+                     "Liverpool", "Manchester City", "Manchester United",
+                     "Newcastle United", "Southampton", "Tottenham Hotspur",
+                     "Watford", "West Ham United", "Wolverhampton Wanderers"))
 
-Arsenal <- cleaned_fifa_data %>%
-  filter(Club %in% c("Arsenal"))
-
-Brighton <- cleaned_fifa_data %>%
-  filter(Club %in% c("Brighton & Hove Albion"))
-
-Bournemouth <- cleaned_fifa_data %>%
-  filter(Club %in% c("Bournemouth"))
-
-Burnley <- cleaned_fifa_data %>%
-  filter(Club %in% c("Burnley"))
-
-Cardiff_City <- cleaned_fifa_data %>%
-  filter(Club %in% c("Cardiff City"))
-
-Chelsea <- cleaned_fifa_data %>%
-  filter(Club %in% c("Chelsea"))
-
-Crystal_Palace <- cleaned_fifa_data %>%
-  filter(Club %in% c("Crystal Palace"))
-         
-Everton <- cleaned_fifa_data %>%
-  filter(Club %in% c("Everton"))
-
-Fulham <- cleaned_fifa_data %>%
-  filter(Club %in% c("Fulham"))
-
-Huddersfield_Town <-  cleaned_fifa_data %>%
-  filter(Club %in% c("Huddersfield Town"))
-
-Leicester <- cleaned_fifa_data %>%
-  filter(Club %in% c("Leicester City"))
-
-Liverpool <- cleaned_fifa_data %>%
-  filter(Club %in% c("Liverpool"))
-
-Manchester_City <- cleaned_fifa_data %>%
-  filter(Club %in% c("Manchester City"))
-
-Manchester_United <- cleaned_fifa_data %>%
-  filter(Club %in% c("Manchester United"))
-
-Newcastle <- cleaned_fifa_data %>%
-  filter(Club %in% c("Newcastle United"))
-
-Southampton <- cleaned_fifa_data %>%
-  filter(Club %in% c("Southampton"))
-
-Tottenham <- cleaned_fifa_data %>%
-  filter(Club %in% c("Tottenham Hotspur"))
-
-Watford <- cleaned_fifa_data %>%
-  filter(Club %in% c("Watford"))
-
-West_Ham <- cleaned_fifa_data %>%
-  filter(Club %in% c("West Ham United"))
-
-Wolves <- cleaned_fifa_data %>%
-  filter(Club %in% c("Wolverhampton Wanderers"))
-         
 ui <- fluidPage(
   selectInput(
     inputId = "team",
     label = "Teams",
     choices = unique(cleaned_fifa_data$Club),
     multiple = TRUE),
-  submitButton(text = "Choose Team"),
-  reactiveValues()
+  uiOutput("players")
 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$players <- renderUI({
+    player_options <- cleaned_fifa_data %>% 
+      filter(Club == input$team) %>%
+      pull(Name)
+    selectInput(inputId = "player_options", 
+                label = "Players",
+                choices = player_options)
+  })
+}
 
 shinyApp(ui = ui, server = server)
 
 #Mission Statement: Finding Player Optimization for different players and teams to see which playing style fits to a
 # certain Fifa player, whether they like Strong players, fast players, good passing, good dribbling, etc.
+# 
+# Arsenal <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Arsenal"))
+# 
+# Brighton <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Brighton & Hove Albion"))
+# 
+# Bournemouth <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Bournemouth"))
+# 
+# Burnley <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Burnley"))
+# 
+# Cardiff_City <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Cardiff City"))
+# 
+# Chelsea <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Chelsea"))
+# 
+# Crystal_Palace <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Crystal Palace"))
+# 
+# Everton <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Everton"))
+# 
+# Fulham <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Fulham"))
+# 
+# Huddersfield_Town <-  cleaned_fifa_data %>%
+#   filter(Club %in% c("Huddersfield Town"))
+# 
+# Leicester <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Leicester City"))
+# 
+# Liverpool <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Liverpool"))
+# 
+# Manchester_City <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Manchester City"))
+# 
+# Manchester_United <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Manchester United"))
+# 
+# Newcastle <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Newcastle United"))
+# 
+# Southampton <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Southampton"))
+# 
+# Tottenham <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Tottenham Hotspur"))
+# 
+# Watford <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Watford"))
+# 
+# West_Ham <- cleaned_fifa_data %>%
+#   filter(Club %in% c("West Ham United"))
+# 
+# Wolves <- cleaned_fifa_data %>%
+#   filter(Club %in% c("Wolverhampton Wanderers"))
+
