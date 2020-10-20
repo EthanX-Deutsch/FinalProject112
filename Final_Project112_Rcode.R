f7247f173297b644089e26b7114b698a07bd4615
jcaskey1 <jcaskey1@macalester.edu>
2020-10-20 09:14:32 -0500

    hover second try

--- Final_Project112_Rcode.R
+++ Final_Project112_Rcode.R
@@ -74,7 +74,7 @@ ui <- fluidPage(
              `Dribbling Ability`)),
   submitButton("Compare Teams"),
   splitLayout(
-    plotOutput("team1"),
+    plotOutput("team1", hover = "plot_text"),
     plotOutput("team2")),
   splitLayout(plotOutput("goalkeep1"),
               plotOutput("goalkeep2"))
